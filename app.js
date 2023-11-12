f25ec35be2b0c530bb4c8cd4c4225b9d77b40626
Faserly App <85167495+faserly@users.noreply.github.com>
2023-11-12 16:48:01 +0300

    install and setup graphql

--- app.js
+++ app.js
@@ -1,13 +1,39 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+import express from 'express';
+import bodyParser from 'body-parser';
+import { graphqlHTTP } from 'express-graphql';
+import { buildSchema } from 'graphql';
 
 const app = express();
 
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-    res.send("Hello World");
-})
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: buildSchema(`
+        type RootQuery {
+            events: [String!]!
+        }
+        type RootMutation {
+            createEvent(name: String): String
+        }
+        schema {
+            query: RootQuery
+            mutation: RootMutation
+        }
+    `),
+    rootValue: {
+      events: () => {
+        return ["bla bla", "bla", "blablabla"];
+      },
+      createEvent: (args) => {
+        const eventName = args.name;
+        return eventName;
+      },
+    },
+    graphiql: true,
+  })
+);
 
 
 app.listen(3000);

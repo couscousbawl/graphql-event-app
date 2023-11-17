2dc2729622ffce13919fc82cd88cb002a323989e
couscousbawl <hamdi.iadh@gmail.com>
2023-11-17 15:40:58 +0300

    adding navigation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,18 +2,22 @@ import { BrowserRouter, Route, Navigate, Routes } from 'react-router-dom';
 import AuthPage from './pages/Auth';
 import EventsPage from './pages/Events';
 import BookingsPage from './pages/Bookings';
+import MainNavigation from './components/Navigation/MainNavigation';
 
 import './App.css'
 
 function App() {
   return (
     <BrowserRouter className="App">
-      <Routes>
-        <Route path="/" element={<Navigate replace to="/auth" />} />
-        <Route path="/auth" element={<AuthPage />} />
-        <Route path="/events" element={<EventsPage />} />
-        <Route path="/bookings" element={<BookingsPage />} />
-      </Routes>
+      <MainNavigation />
+      <div className='main-content'>
+        <Routes>
+          <Route path="/" element={<Navigate replace to="/auth" />} />
+          <Route path="/auth" element={<AuthPage />} />
+          <Route path="/events" element={<EventsPage />} />
+          <Route path="/bookings" element={<BookingsPage />} />
+        </Routes>
+      </div>
     </BrowserRouter>
   );
 }

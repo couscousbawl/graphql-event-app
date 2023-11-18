231dd80a09f60b2121114b5c77b77c6f2ceb4a11
couscousbawl <hamdi.iadh@gmail.com>
2023-11-18 16:48:13 +0300

    integration with backend

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import bodyParser from 'body-parser';
 import { graphqlHTTP } from 'express-graphql';
 import mongoose from 'mongoose';
@@ -10,6 +11,13 @@ import isAuth from './middleware/isAuth.js';
 const app = express();
 
 app.use(bodyParser.json());
+
+app.use(cors({
+  origin: "*",
+  optionsSuccessStatus: 200,
+  methods: ['GET', 'POST', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+}));
 
 app.use(isAuth);
 
@@ -27,8 +35,8 @@ mongoose
     `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.2xn3e3v.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`
   )
   .then(() => {
-    app.listen(3000);
-    console.log('app listening at: 3000')
+    app.listen(4000);
+    console.log('app listening at: 4000')
   })
   .catch((err) => {
     console.log(err);
